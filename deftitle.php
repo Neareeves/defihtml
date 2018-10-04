b0c3a0864a97e5a8b8612e224e1f53b130a8846c
Jaune Loke <Jaune.Loke@gmail.com>
2018-10-04 15:39:26 +0200

    ajout projet Nathan

--- deftitle.php
+++ deftitle.php
@@ -1,94 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-
-		<meta charset="utf-8" />
-
-		<meta name="description" content="On vous explique les balises HTML sans se taper la tête contre le clavier! ">
-		<meta name="author" content="Une équipe de super développeurs!">
-
-		<link rel="shortcut icon" type="image/png" href="logoHtml.png">
-		<link rel="stylesheet" type="text/css" href="stylesa.css">
-		<link rel="stylesheet" type="text/css" href="stylesk.css">
-		<link rel="stylesheet" type="text/css" href="styleN.css">
-		<link rel="stylesheet" type="text/css" href="stylese.css">
-
-		<title>Balise &lsaquo;Title&rsaquo;</title>
+<?php include('head.php') ; ?> 
+<title>Balise &lsaquo;title&rsaquo;</title>
 
 </head>
 <body>
-	<header>
-	<div id="conteneur">
-			<div class="accroche">Explication des balises HTML<br>Garantie sans tapage de tête contre le clavier !</div>
-			<div class="titrepage"><h1>Les balises HTML</h1></div>
-			<div class="logogroupe"><img src="logoGroupe100.png" alt="logo de la meilleure équipe"></div>
-		</div>
-
-		
-
-
-	
-
-	<nav>
-		<ul>
-			<li class="menu-block"><a href="block.html">Balises block</a>
-
-				<ul class="sousmenu">
-
-					<li><a href="def_footer.php">La balise &lsaquo;footer&rsaquo;</a></li>
-
-					<li><a href="def_form.php">La balise &lsaquo;form&rsaquo;</a></li>
-
-					<li><a href="defhr.php">La balise &lsaquo;hr&rsaquo;</a></li>
-
-					<li><a href="def_nav.php">La balise &lsaquo;nav&rsaquo;</a></li>
-					
-					<li><a href="defsection.php">La balise &lsaquo;section&rsaquo;</a></li>
-					
-					<li><a href="def_table.php">La balise &lsaquo;table&rsaquo;</a></li>
-
-					<li><a href="def_textarea.php">La balise &lsaquo;textarea&rsaquo;</a></li>
-					
-				</ul>
-			</li>		
-			<li class="menu-inline"><a href="inline.html">Balises inline</a>
-
-				<ul class="sousmenu">
-
-					<li><a href="defa.php"> La balise &lsaquo;a&rsaquo; </a></li>
-
-					<li><a href="defi.php">La balise &lsaquo;i&rsaquo;</a></li>
-
-					<li><a href="defimg.php">La balise &lsaquo;img&rsaquo;</a></li>
-
-					<li><a href="def_link.php">La balise &lsaquo;link&rsaquo;</a></li>
-
-					
-				</ul>
-			</li>
-			<li class="menu-speciales"><a href="menu-speciales">Balises spéciales</a>
-
-				<ul class="sousmenu">
-
-					<li><a href="def_commentaires.php">La balise &lsaquo;!...&rsaquo;</a></li>
-
-					<li><a href="defbody.php">La balise &lsaquo;body&rsaquo;</a></li>
-					
-					<li><a href="defheader.php">La balise &lsaquo;header&rsaquo;</a></li>
-					
-					<li><a href="def_meta.php">La balise &lsaquo;meta&rsaquo;</a></li>
-					
-					<li><a href="deftitle.php">La balise &lsaquo;title&rsaquo;</a></li>
-				
-				</ul>
-			</li>
-		</ul>
-	</nav>
-
-	<!-- <img src="logoGroupe50.png" alt="logo de la meilleure équipe"> -->
-		<!-- Menu déroulant, nom du site -->
-
-	</header>
+	<?php include('navigateur.php') ; ?> 
 	
 	
 		<main>
@@ -128,15 +43,7 @@
 
 			</article>
 		</main>
-	<footer>
+		<?php include('navigateurbas.php') ; ?> 
 
-		<div id ="logoAHC">
-			<div class="logoACS"> <img src="logoACS.png" alt=logoACS> </div>
-			<div class="nomEquipe"> Ce site à été rédigé par FRANCOIS Nathan, ARRIAS CAMPS Aleth, BERNARD Kevin, QUINONERO Emilie.</div>
-			<div class="logoCH"> <img src="Html5.png" alt=logoHTML> <img src="CSS3.png" alt=logoCSS> </div>
-			<div class="retourAccueil"> <a href="accueil.html"><img src="boutonRetour2.png" alt="Accueil" title="ACCUEIL"> </a></div>
-		</div>
-
-	</footer>
 </body>
 </html>
