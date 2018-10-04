39cb30a39c90aec7f7260df58b7a071de0733932
Neareeves <alethesj@gmail.com>
2018-10-04 14:53:00 +0200

    nagitateur

--- navigateur.php
+++ navigateur.php
@@ -13,7 +13,7 @@
 		<link rel="stylesheet" type="text/css" href="styleN.css">
 		<link rel="stylesheet" type="text/css" href="stylese.css">
 
-		<title>Balise &lsaquo;i&rsaquo;</title>
+		<title>Les balises HTML</title>
 
 </head>
 <body>
