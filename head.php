6711fc76802f4cbcd02ca19589568d5ab6393c64
kvnbrnrd <kevos.directos@gmail.com>
2018-10-04 15:29:53 +0200

    ajout head.php

---
+++ head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+		<meta charset="utf-8" />
+
+		<meta name="description" content="On vous explique les balises HTML sans se taper la tête contre le clavier! ">
+		<meta name="author" content="Une équipe de super développeurs!">
+
+		<link rel="shortcut icon" type="image/png" href="logoHtml.png">
+		<link rel="stylesheet" type="text/css" href="stylesa.css">
+		<link rel="stylesheet" type="text/css" href="stylesk.css">
+		<link rel="stylesheet" type="text/css" href="styleN.css">
+		<link rel="stylesheet" type="text/css" href="stylese.css">
