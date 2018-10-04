b7239514b620c9bcb346b91162e339a0608d42c8
Neareeves <alethesj@gmail.com>
2018-10-04 14:25:43 +0200

    nagitateur

--- defi.php
+++ defi.php
@@ -2,13 +2,15 @@
 	
 <?php include('navigateur.php') ; ?>
 
-
+<main>
 			<h2 class="letitre">La balise &lsaquo;i&rsaquo;
 			</h2>
 
 			<article>
 
+			<div class="clear"></div>
 				<!-- <h3 class="titredefinition">Définition</h3> -->
+					<div class="leschevrons">
 
 						<p class="textecourant">La balise &lsaquo;i&rsaquo; est un élément in-line qui met la partie de texte entre sa balise d'ouverture et sa balise de fermeture en italique. Actuellement, elle n'est plus utilisée seulement pour l'italique, mais pour identifier un morceau de texte qui se différencie du reste du texte, comme un titre, un mot dans une langue étrangère ou une citation. L'affichage en italique est l'affichage par défaut.</p>
 
