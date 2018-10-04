29819f88897eccd7acc6250bb818d101c2894078
kvnbrnrd <kevos.directos@gmail.com>
2018-10-04 14:32:30 +0200

    ajout header php

---
+++ navigateurbas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+		<meta charset="utf-8" />
+
+		<meta name="description" content="On vous explique les balises HTML sans se taper la tête contre le clavier! ">
+		<meta name="author" content="Une équipe de super développeurs!">
+
+		<link rel="shortcut icon" type="image/png" href="logoHtml.png">
+		<link rel="stylesheet" type="text/css" href="stylesa.css">
+		<link rel="stylesheet" type="text/css" href="stylesk.css">
+		<link rel="stylesheet" type="text/css" href="styleN.css">
+		<link rel="stylesheet" type="text/css" href="stylese.css">
+
+		<title>Balise &lsaquo;nav&rsaquo;</title>
+
+</head>
+<body>
+
+<footer>
+
+<div id ="logoAHC">
+    <div class="logoACS"> <img src="logoACS.png" alt=logoACS> </div>
+    <div class="nomEquipe"> Ce site à été rédigé par FRANCOIS Nathan, ARRIAS CAMPS Aleth, BERNARD Kevin, QUINONERO Emilie.</div>
+    <div class="logoCH"> <img src="Html5.png" alt=logoHTML> <img src="CSS3.png" alt=logoCSS> </div>
+    <div class="retourAccueil"> <a href="accueil.html"><img src="boutonRetour2.png" alt="Accueil" title="ACCUEIL"> </a></div>
+</div>
+
+</footer>
