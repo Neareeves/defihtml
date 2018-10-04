2b26856932a7e5d9e2da830f570f8586d8f88b77
Jaune Loke <Jaune.Loke@gmail.com>
2018-10-04 14:14:40 +0200

    maj de phhhhhhpp

---
+++ def_link.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+		<meta charset="utf-8" />
+
+		<meta name="description" content="On vous explique les balises HTML sans se taper la tête contre le clavier! ">
+		<meta name="author" content="Une équipe de super développeurs!">
+
+		<link rel="shortcut icon" type="image/png" href="logoHtml.png">
+		<link rel="stylesheet" type="text/css" href="stylesa.css">
+		<link rel="stylesheet" type="text/css" href="stylesk.css">
+		<link rel="stylesheet" type="text/css" href="styleN.css">
+		<link rel="stylesheet" type="text/css" href="stylese.css">
+
+		<title>Balise &lsaquo;link&rsaquo;</title>
+
+</head>
+<body>
+	<header>
+		<div id="conteneur">
+			<div class="accroche">Explication des balises HTML<br>Garantie sans tapage de tête contre le clavier !</div>
+			<div class="titrepage"><h1>Les balises HTML</h1></div>
+			<div class="logogroupe"><img src="logoGroupe100.png" alt="logo de la meilleure équipe"></div>
+		</div>
+
+		
+
+
+	
+
+	<nav>
+		<ul>
+			<li class="menu-block"><a href="block.html">Balises block</a>
+
+				<ul class="sousmenu">
+
+					<li><a href="def_footer.html">La balise &lsaquo;footer&rsaquo;</a></li>
+
+					<li><a href="def_form.html">La balise &lsaquo;form&rsaquo;</a></li>
+
+					<li><a href="defhr.html">La balise &lsaquo;hr&rsaquo;</a></li>
+
+					<li><a href="def_nav.html">La balise &lsaquo;nav&rsaquo;</a></li>
+					
+					<li><a href="defsection.html">La balise &lsaquo;section&rsaquo;</a></li>
+					
+					<li><a href="def_table.html">La balise &lsaquo;table&rsaquo;</a></li>
+
+					<li><a href="def_textarea.html">La balise &lsaquo;textarea&rsaquo;</a></li>
+					
+				</ul>
+			</li>		
+			<li class="menu-inline"><a href="inline.html">Balises inline</a>
+
+				<ul class="sousmenu">
+
+					<li><a href="defa.html"> La balise &lsaquo;a&rsaquo; </a></li>
+
+					<li><a href="defi.html">La balise &lsaquo;i&rsaquo;</a></li>
+
+					<li><a href="defimg.html">La balise &lsaquo;img&rsaquo;</a></li>
+
+					<li><a href="def_link.html">La balise &lsaquo;link&rsaquo;</a></li>
+
+					
+				</ul>
+			</li>
+			<li class="menu-speciales"><a href="menu-speciales">Balises spéciales</a>
+
+				<ul class="sousmenu">
+
+					<li><a href="def_commentaires.html">La balise &lsaquo;!...&rsaquo;</a></li>
+
+					<li><a href="defbody.html">La balise &lsaquo;body&rsaquo;</a></li>
+					
+					<li><a href="defheader.html">La balise &lsaquo;header&rsaquo;</a></li>
+					
+					<li><a href="def_meta.html">La balise &lsaquo;meta&rsaquo;</a></li>
+					
+					<li><a href="deftitle.html">La balise &lsaquo;title&rsaquo;</a></li>
+				
+				</ul>
+			</li>
+		</ul>
+	</nav>
+
+	<!-- <img src="logoGroupe50.png" alt="logo de la meilleure équipe"> -->
+		<!-- Menu déroulant, nom du site -->
+
+	</header>
+	
+		<main>
+
+			<h2 class="letitre">La balise &lsaquo;link&rsaquo;
+			</h2>
+
+			<article>
+					<div class="clear"></div>
+				<!-- <h3 class="titredefinition">Définition</h3> -->
+					<div class="leschevrons">
+						<p class="textecourant">Définit la relation entre le document courant et une ressource externe. Cet élément peut être utilisé pour définir un lien vers une feuille de style ou un cadre de navigation (accéder à la même page dans une langue différente par exemple).
+						</p>
+					</div>
+				<div class="exemples">
+					<h4>Exemple : </h4>
+
+					<div>
+					&lsaquo; link &rsaquo; peut lier le fichier html vers différentes feuilles de style qui permettra de gèrer le design du site.  Ces liens apparaitront sur l'éditeur de code mais seront invisible pour l'utilisateur.
+					</div>
+
+					<br>
+
+					<img src="deflinkillustration.png" alt="Exemple de code avec la balise link">
+
+					<br>
+
+					<!-- <div>
+					Et un style de lien hypertexte se met par défaut.
+					</div> 
+					
+					<br>
+
+					<img src="defaillus2.jpg" alt="Illustration de l'utilisation de la balise a.">		
+				</div> -->
+				<br>
+				<div class="clear"></div>
+			</article>
+		</main>
+
+	<footer>
+		<div id ="logoAHC">
+			<div class="logoACS"> <img src="logoACS.png" alt=logoACS> </div>
+			<div class="nomEquipe"> Ce site à été rédigé par FRANCOIS Nathan, ARRIAS CAMPS Aleth, BERNARD Kevin, QUINONERO Emilie. </div>
+			<div class="logoCH"> <img src="HTML5.png" alt=logoHTML> <img src="CSS3.png" alt=logoCSS> </div>
+			<div class="retourAccueil"> <a href="accueil.html"><img src="boutonRetour2.png" alt="Accueil" title="ACCUEIL"> </a>
+			</div>
+		</div>
+			<!-- Logos HTML5, CSS3, ACS, développeurs -->
+	</footer>
+
+</body>
+</html>
---
+++ defi.php
@@ -0,0 +1,49 @@
+<?php include('/navigateur.php'); ?>
+	
+		<main>
+
+			<h2 class="letitre">La balise &lsaquo;i&rsaquo;
+			</h2>
+
+			<article>
+
+				<!-- <h3 class="titredefinition">Définition</h3> -->
+					<div class="leschevrons">
+						<p class="textecourant">La balise &lsaquo;i&rsaquo; est un élément in-line qui met la partie de texte entre sa balise d'ouverture et sa balise de fermeture en italique.</p>
+					</div>
+				<div class="exemples">	
+					<h4>Exemple : </h4>
+
+					<div>
+				Des balises i sont placées de chaque côté de l'expression que je veux mettre en italique.<!-- Insérer text code  -->
+					</div>
+
+					<br>
+
+					<img src="defiillus1.jpg" alt="Illustration de la balise i.">
+
+						<br><br>
+					<div>
+					Les mots placés entre la balise sont en italique par défaut.<!-- Insérer texte résultat -->
+					</div>
+
+					<br>
+
+				<img src="defiillus2.jpg" alt="Illustration 	de l'effet de la balise i.">	
+				</div>
+				<br>	
+			</article>
+		</main>
+	<footer>
+		<div id ="logoAHC">
+			<div class="logoACS"> <img src="logoACS.png" alt=logoACS> </div>
+			<div class="nomEquipe"> Ce site à été rédigé par FRANCOIS Nathan, ARRIAS CAMPS Aleth, BERNARD Kevin, QUINONERO Emilie. </div>
+			<div class="logoCH"> <img src="HTML5.png" alt=logoHTML> <img src="CSS3.png" alt=logoCSS> </div>
+			<div class="retourAccueil"> <a href="accueil.html"><img src="boutonRetour2.png" alt="Accueil" title="ACCUEIL"> </a>
+			</div>
+		</div>
+			<!-- Logos HTML5, CSS3, ACS, développeurs -->
+	</footer>
+
+</body>
+</html>
