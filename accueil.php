0f3d322560576b4565133a3ebd383c034163d7e5
kvnbrnrd <kevos.directos@gmail.com>
2018-10-05 10:25:40 +0200

    accueil

--- accueil.php
+++ accueil.php
@@ -13,8 +13,8 @@
 	<div class="leschevrons">
 		<p>Les balises HTML sont les éléments de base du codage HTML d’une page web. Invisibles pour l’internaute (sauf en activant l’affichage du code source), elles sont interprétées par le navigateur pour la composition de la page.
 
-		Elle permettent la mise en forme et la structuration des contenus et certaines sont spécifiquement prises en compte par les algorithmes des moteurs de recherche pour l’établissement du score de la page sur une requête et la restitution des résultats.
-		A l’aide de ce site vous pourrez découvrir certaines des balises HTML les plus couramment utilisés.
+		Elles permettent la mise en forme et la structuration des contenus et certaines sont spécifiquement prises en compte par les algorithmes des moteurs de recherche pour l’établissement du score de la page sur une requête et pour la restitution des résultats.
+		A l’aide de ce site vous pourrez découvrir certaines des balises HTML les plus courantes.
 		</p>
 	</div>
 	<br><br>
