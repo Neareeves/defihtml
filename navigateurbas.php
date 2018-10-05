ff29d64404dc10eb4c0a0665dadcd72ac7d25d9c
Neareeves <alethesj@gmail.com>
2018-10-05 09:10:45 +0200

    nagitateur

--- navigateurbas.php
+++ navigateurbas.php
@@ -2,7 +2,7 @@
 
 <div id ="logoAHC">
     <div class="logoACS"> <img src="logoACS.png" alt=logoACS> </div>
-    <div class="nomEquipe"> Ce site à été rédigé par FRANCOIS Nathan, ARRIAS CAMPS Aleth, BERNARD Kevin, QUINONERO Emilie.</div>
+    <div class="nomEquipe"> Ce site à été rédigé par Nathan FRANCOIS, Aleth ARRIAS CAMPS, Kevin BERNARD, Émilie QUINONERO.</div>
     <div class="logoCH"> <img src="Html5.png" alt=logoHTML> <img src="CSS3.png" alt=logoCSS> </div>
     <div class="retourAccueil"> <a href="accueil.html"><img src="boutonRetour2.png" alt="Accueil" title="ACCUEIL"> </a></div>
 </div>
